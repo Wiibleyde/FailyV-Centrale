6d39fc6eea32c643f086e28e59264db427d3827e
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-13 02:36:53 +0200

    Changed to a real ping command

--- commands/test/ping.js
+++ commands/test/ping.js
@@ -1,5 +1,9 @@
 //Récupération des fonctions pour créer une commande
 const { SlashCommandBuilder } = require('discord.js');
+//Récup du logger
+const logger = require('./../../modules/logger');
+//Récup du créateur d'embed
+const emb = require('./../../modules/embeds');
 
 module.exports = {
     //Création de la commande
@@ -7,7 +11,12 @@ module.exports = {
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
-    //Action lors de l'execution de la commande
-        await interaction.reply({ content: 'Pong!', ephemeral: true });
+        //Affichage du message "Iris réfléchis..."
+        await interaction.deferReply({ ephemeral: true });
+        //Format de la date de démarrage
+        var time = new Date(`${logger.getStartDate()} GMT+2:00`);
+        var logged_at = Math.floor(time / 1000);
+        //Action lors de l'execution de la commande
+        await interaction.followUp({ embeds: [emb.generate(null, null, `🏓 Pong ! \n- **Ping :** ${Date.now() - interaction.createdTimestamp} ms \n- **Latence :** ${Math.round(interaction.client.ws.ping)}ms \n- **Mémoire :** ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100} MB \n- **En ligne depuis : <t:${logged_at}:R>** `, `Gold`, null, null, null, null, null, interaction.client.user.username, interaction.client.user.avatarURL(), true)], ephemeral: true });
     },
 };

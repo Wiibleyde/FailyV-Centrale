0019d1143eb0b74c9ee34cf023502fcc5b594df7
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-14 06:57:49 +0200

    "Fixed" service & dispatch count display in rpc

--- events/ready.js
+++ events/ready.js
@@ -16,17 +16,20 @@ module.exports = {
         //Récupération des personnes en service
         const guild = client.guilds.cache.get(process.env.IRIS_PRIVATE_GUILD_ID);
         await guild.members.fetch();
-        const countPDS = guild.roles.cache.get(process.env.IRIS_SERVICE_ROLE_ID).members;
-        var serviceCount = countPDS.size;
-        const countDispatch = guild.roles.cache.get(process.env.IRIS_DISPATCH_ROLE_ID).members;
-        var dispatchCount = countDispatch.size;
+        var serviceCount = guild.roles.cache.get(process.env.IRIS_SERVICE_ROLE_ID).members.size;
+        var dispatchCount = guild.roles.cache.get(process.env.IRIS_DISPATCH_ROLE_ID).members.size;
         //Affichage de l'activitée du bot
         if(process.env.MODE == 'DEV') { client.user.setPresence({ activities: [{ name: `🤖 Dev mode!`, type: ActivityType.Competing }], status: 'idle' }); }
         else if(process.env.MODE == 'MAINTENANCE'){ client.user.setPresence({ activities: [{ name: `Maintenance en cours...`, type: ActivityType.Watching }], status: 'dnd' }); }
         else {
-            setInterval(() => {
+            client.user.setPresence({ activities: [{ name: `🚑 ` + serviceCount + ` | 🎙️ ` + dispatchCount, type: ActivityType.Watching }], status: 'online' });
+            setInterval(async () => {
+                //Récupération des personnes en service
+                await guild.members.fetch();
+                serviceCount = guild.roles.cache.get(process.env.IRIS_SERVICE_ROLE_ID).members.size;
+                dispatchCount = guild.roles.cache.get(process.env.IRIS_DISPATCH_ROLE_ID).members.size;
                 client.user.setPresence({ activities: [{ name: `🚑 ` + serviceCount + ` | 🎙️ ` + dispatchCount, type: ActivityType.Watching }], status: 'online' });
-            }, 1000);
+            }, 5000);
         }
         service.start(client);
     },

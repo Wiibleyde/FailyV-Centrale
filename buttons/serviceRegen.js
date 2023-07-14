cb9fe0d0d07e419ee9c24414b9dccfeef0ae89ad
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-15 00:08:02 +0200

    Ping users on radio change

--- buttons/serviceRegen.js
+++ buttons/serviceRegen.js
@@ -12,6 +12,7 @@ const serviceID = process.env.IRIS_SERVICE_ROLE_ID;
 module.exports = {
     execute: async function(interaction, errEmb) {
         if(interaction.member.roles.cache.has(serviceID)) {
+            var pingMsg = `<@&${serviceID}> changement de fréquence radio `;
             //Generation aléatoire de la radio entre 250.0 et 344.9
             const freqUnit = Math.floor(Math.random() * (344-250+1)) + 250;
             const freqDeci = Math.floor(Math.random() * 10);
@@ -22,8 +23,8 @@ module.exports = {
             var freqLSMS = interaction.message.embeds[0].fields[0].value;
             var freqFDO = interaction.message.embeds[0].fields[1].value;
             //Changement de la fréquence si radio LSMS régen
-            if(interaction.customId == 'serviceRegenLSMS') { freqLSMS = freq; await sql.setRadio('lsms', freqLSMS); }
-            if(interaction.customId == 'serviceRegenFDO') { freqFDO = freq; }
+            if(interaction.customId == 'serviceRegenLSMS') { freqLSMS = freq; await sql.setRadio('lsms', freqLSMS); pingMsg = pingMsg + 'LSMS !'; }
+            if(interaction.customId == 'serviceRegenFDO') { freqFDO = freq; pingMsg = pingMsg + 'commune LSMS - FDO !'; }
             //Ajout des radios
             embed.addFields([
                 {
@@ -49,9 +50,9 @@ module.exports = {
             var genEvent = await sql.isRadioDisplayed('event');
             genEvent = genEvent[0].displayed;
             //Changement de la fréquence si radio BCMS régen
-            if(interaction.customId == 'serviceRegenBCMS') { freqBCMS = freq; genBCMS = '1'; await sql.updatedRadioDisplay('bcms', '1'); await sql.setRadio('bcms', freqBCMS); freqToRegen = 'bcms'; }
+            if(interaction.customId == 'serviceRegenBCMS') { freqBCMS = freq; genBCMS = '1'; await sql.updatedRadioDisplay('bcms', '1'); await sql.setRadio('bcms', freqBCMS); freqToRegen = 'bcms'; pingMsg = pingMsg + 'LSMS - BCMS !'; }
             //Changement de la fréquence si radio Event régen
-            if(interaction.customId == 'serviceRegenEvent') { freqEvent = freq; genEvent = '1'; await sql.updatedRadioDisplay('event', '1'); await sql.setRadio('event', freqEvent); freqToRegen = 'event'; }
+            if(interaction.customId == 'serviceRegenEvent') { freqEvent = freq; genEvent = '1'; await sql.updatedRadioDisplay('event', '1'); await sql.setRadio('event', freqEvent); freqToRegen = 'event'; pingMsg = pingMsg + 'évènementiel !'; }
             if(genBCMS == '1' && genEvent == '0') {
                 //Récupération de la radio BCMS si non régénérée
                 if(interaction.message.embeds[0].fields[3] != null) {
@@ -147,6 +148,10 @@ module.exports = {
             await interaction.message.edit({ embeds: [embed], components: [interaction.message.components[0]] });
             //Confirmation à Discord du succès de l'opération
             await interaction.deferUpdate();
+            const pingMessage = await interaction.channel.send({ content: pingMsg });
+            // Supprime la réponse après 1min
+            await wait(60000);
+            await pingMessage.delete();
         } else {
             await interaction.reply({ embeds: [emb.generate(`Action impossible !`, null, `Désolé, vous devez être en service pour régénérer une radio !`, `#ff0000`, null, null, null, null, null, interaction.client.user.username, interaction.client.user.avatarURL(), true)], ephemeral: true });
             // Supprime la réponse après 5s

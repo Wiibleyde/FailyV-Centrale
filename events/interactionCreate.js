eb310112da444bf23e330fbc67dc64e5201feadb
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-14 23:01:44 +0200

    Added btns & removed a select menu

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -50,9 +50,11 @@ module.exports = {
             if(interaction.customId == 'checkDebug') { const checkDebug = require('./../buttons/checkDebug'); checkDebug.execute(interaction, errEmb); }
             if(interaction.customId == 'denyDebug') { const denyDebug = require('./../buttons/denyDebug'); denyDebug.execute(interaction, errEmb); }
             if(interaction.customId == 'serviceRegenLSMS') { const serviceRegen = require('./../buttons/serviceRegen'); serviceRegen.execute(interaction, errEmb); }
+            if(interaction.customId == 'serviceRegenFDO') { const serviceRegen = require('./../buttons/serviceRegen'); serviceRegen.execute(interaction, errEmb); }
             if(interaction.customId == 'serviceRegenBCMS') { const serviceRegen = require('./../buttons/serviceRegen'); serviceRegen.execute(interaction, errEmb); }
             if(interaction.customId == 'serviceRegenEvent') { const serviceRegen = require('./../buttons/serviceRegen'); serviceRegen.execute(interaction, errEmb); }
             if(interaction.customId == 'serviceManage') { const serviceManage = require('./../buttons/serviceManage'); serviceManage.execute(interaction, errEmb); }
+            if(interaction.customId == 'serviceRadioReset') { const serviceRadioReset = require('./../buttons/serviceRadioReset'); serviceRadioReset.execute(interaction, errEmb); }
             if(interaction.customId == 'serviceSwitch') { const serviceSwitch = require('./../buttons/serviceSwitch'); serviceSwitch.execute(interaction, errEmb); }
             if(interaction.customId == 'serviceDispatch') { const serviceDispatch = require('./../buttons/serviceDispatch'); serviceDispatch.execute(interaction, errEmb); }
             if(interaction.customId == 'serviceSwitchOff') { const serviceSwitchOff = require('./../buttons/serviceSwitchOff'); serviceSwitchOff.execute(interaction, errEmb); }
@@ -68,7 +70,6 @@ module.exports = {
             //Pré-écriture d'un message d'erreur (pour éviter de l'écrire dans tout les fichers)
             const errEmb = emb.generate(`Oups! Une erreur s'est produite :(`, null, `Il semblerait qu'une erreur se soit produite lors de l'interaction avec le menu de séléction, si le problème persiste n'hésitez pas à faire une demande de débug via le </debug:${process.env.IRIS_DEBUG_COMMAND_ID}> avec le plus de détails possible ! (Merci d'avance <:green_heart:1112687922651594762>)`, `#ff0000`, null, null, null, null, null, interaction.client.user.username, interaction.client.user.avatarURL(), true);
             //Appel du fichier spécifique pour chaques interactions
-            if(interaction.customId == 'serviceManageSelect') { const serviceManageSelect = require('./../selectMenus/serviceManageSelect'); serviceManageSelect.execute(interaction, errEmb); }
             if(interaction.customId == 'serviceKickSingleSelect') { const serviceKickSingleSelect = require('./../selectMenus/serviceKickSingleSelect'); serviceKickSingleSelect.execute(interaction, errEmb); }
         }
 

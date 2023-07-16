d25dfcc1711a96176f11f561b705f082f1fb6dcd
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-16 07:37:08 +0200

    Security to not remove two patient at same time

--- buttons/btnsLit.js
+++ buttons/btnsLit.js
@@ -16,27 +16,34 @@ const btnCreator = require('./../modules/btnCreator');
 
 module.exports = {
     execute: async function(interaction, errEmb) {
-        await interaction.deferReply({ ephemeral: true });
-        const letter = interaction.customId;
+        if(!service.isGen()) {
+            await interaction.deferReply({ ephemeral: true });
+            const letter = interaction.customId;
 
-        service.setGen(true);
-        await beds.remove(letter);
+            service.setGen(true);
+            await beds.remove(letter);
 
-        let lits = await beds.get();
-        let patient = [];
-        let patientLetter = [];
-        let patientSurveilance = [];
-        for(i=0;i<lits.length;i++) {
-            patient.push(lits[i].patient);
-            patientLetter.push(lits[i].letter);
-            patientSurveilance.push(lits[i].surveillance);
+            let lits = await beds.get();
+            let patient = [];
+            let patientLetter = [];
+            let patientSurveilance = [];
+            for(i=0;i<lits.length;i++) {
+                patient.push(lits[i].patient);
+                patientLetter.push(lits[i].letter);
+                patientSurveilance.push(lits[i].surveillance);
+            }
+            const imgMsg = await img.write(patient, patientLetter, patientSurveilance, interaction.client);
+            let imgUrl;
+            imgMsg.attachments.map(bedImg => imgUrl = bedImg.attachment);
+
+            let letters = await beds.getLetters();
+            editBedsImage(letters, interaction.message, imgUrl, interaction);
+        } else {
+            await interaction.reply({ embeds: [emb.generate(null, null, `L'aperçu de la salle de réveil est déjà en cours de mise à jour, veuillez patienter quelques secondes !`, `#FEAC12`, process.env.LSMS_LOGO_V2, null, `Gestion de la salle de réveil`, `https://cdn.discordapp.com/icons/${process.env.IRIS_PRIVATE_GUILD_ID}/${interaction.client.guilds.cache.get(process.env.IRIS_PRIVATE_GUILD_ID).icon}.webp`, null, null, null, true)], ephemeral: true });
+            // Supprime la réponse après 5s
+            await wait(5000);
+            await interaction.deleteReply();
         }
-        const imgMsg = await img.write(patient, patientLetter, patientSurveilance, interaction.client);
-        let imgUrl;
-        imgMsg.attachments.map(bedImg => imgUrl = bedImg.attachment);
-
-        let letters = await beds.getLetters();
-        editBedsImage(letters, interaction.message, imgUrl, interaction);
     }
 }
 

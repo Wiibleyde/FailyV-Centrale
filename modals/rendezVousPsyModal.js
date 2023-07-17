37108317a83d075a0644cb2fb831deccf60786eb
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-07-17 18:34:23 +0200

    Fix rdv phone number + remove useless buttons
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- modals/rendezVousPsyModal.js
+++ modals/rendezVousPsyModal.js
@@ -8,17 +8,39 @@ const emb = require('../modules/embeds');
 module.exports = {
     //Création de la commande
     execute: async function(interaction, errEmb) {
+        //Regex des numéros de téléphone
+        const regExpFull = new RegExp("^555-[0-9]{4}$");
+        const regExpMidFull = new RegExp("^555[0-9]{4}$");
+        const regExp = new RegExp("^[0-9]{4}$");
+        //Check if phone number is valid
+        if (!regExpFull.test(interaction.components[1].components[0].value) && !regExpMidFull.test(interaction.components[1].components[0].value) && !regExp.test(interaction.components[1].components[0].value)) {
+            try {
+                await interaction.reply({ content: `Le numéro de téléphone ${interaction.components[1].components[0].value} n'est pas valide. Veuillez entrer un numéro de téléphone valide (555-5420 ou 5555420 ou 5420).`, ephemeral: true });
+            } catch (e) {
+                logger.error(e);
+            }
+            return;
+        }
+        let phoneNumber = "";
+        if (regExpFull.test(interaction.components[1].components[0].value)) {
+            phoneNumber = interaction.components[1].components[0].value;
+        } else if (regExpMidFull.test(interaction.components[1].components[0].value)) {
+            phoneNumber = interaction.components[1].components[0].value.substring(3);
+            phoneNumber = `555-${phoneNumber}`;
+        } else if (regExp.test(interaction.components[1].components[0].value)) {
+            phoneNumber = `555-${interaction.components[1].components[0].value}`;
+        }
         //Get user guild pseudo
         const pseudo = interaction.member.displayName;
         //Create embed
-        const rendezVousEmb = emb.generate(null, null, `**Nom et prénom:** ${interaction.components[0].components[0].value}\n**Numéro de téléphone:** ${interaction.components[1].components[0].value}\n**Description:** ${interaction.components[2].components[0].value}\n**Contacté:** 0 fois\n**Pris par:** ${pseudo}`, `#FF0000`, process.env.LSMS_LOGO_V2, null, `Rendez-vous psychologique`, `https://cdn.discordapp.com/attachments/1083724872045297734/1124914370217005127/LSMS.png`, null, null, null, false);
+        const rendezVousEmb = emb.generate(null, null, `**Nom et prénom:** ${interaction.components[0].components[0].value}\n**Numéro de téléphone:** ${phoneNumber}\n**Description:** ${interaction.components[2].components[0].value}\n**Contacté:** 0 fois\n**Pris par:**`, `#FF0000`, process.env.LSMS_LOGO_V2, null, `Rendez-vous psychologique`, `https://cdn.discordapp.com/attachments/1083724872045297734/1124914370217005127/LSMS.png`, null, pseudo, null, false);
         //Get channel by looking at env var
         const channelToSend = interaction.guild.channels.cache.get(process.env.IRIS_PSYCHO_CHANNEL_ID);
         //Ajout des boutons sous l'embed pour : Dire que le rendez vous est fini, que la personne a été contactée, que le rendez-vous a été pris/que la date a été fixée, que le rendez-vous a été annulé
         const rendezVousActionRow = new ActionRowBuilder().addComponents(
             new ButtonBuilder().setCustomId('rendezVousFini').setLabel("Rendez-vous fini").setStyle(ButtonStyle.Success).setEmoji("✅"),
-            new ButtonBuilder().setCustomId('rendezVousContacte').setLabel("Personne contactée").setStyle(ButtonStyle.Secondary).setEmoji("📞"),
-            new ButtonBuilder().setCustomId('rendezVousPris').setLabel("Rendez-vous pris").setStyle(ButtonStyle.Primary).setEmoji("📆"),
+            // new ButtonBuilder().setCustomId('rendezVousContacte').setLabel("Personne contactée").setStyle(ButtonStyle.Secondary).setEmoji("📞"),
+            // new ButtonBuilder().setCustomId('rendezVousPris').setLabel("Rendez-vous pris").setStyle(ButtonStyle.Primary).setEmoji("📆"),
             new ButtonBuilder().setCustomId('rendezVousAnnule').setLabel("Rendez-vous annulé").setStyle(ButtonStyle.Danger).setEmoji("❌")
         );
         //Send embed with buttons

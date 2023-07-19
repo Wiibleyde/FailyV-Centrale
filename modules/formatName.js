8a51475b01e179a9b709267325395c63dbf3abf4
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-19 16:51:15 +0200

    Fixed weird display of second part name

--- modules/formatName.js
+++ modules/formatName.js
@@ -2,7 +2,6 @@ module.exports = {
     name: (nameToFormat) => {
         if(nameToFormat != null) {
             let name = nameToFormat.charAt(0).toUpperCase() + nameToFormat.slice(1);
-            if(nameToFormat.length > 2) {
                 if(name.includes('-')) {
                     for(j=0;j<name.length;j++) {
                         if(name.charAt(j).includes('-')) {
@@ -10,18 +9,13 @@ module.exports = {
                         }
                     }
                 }
-                for(j=2;j<nameToFormat.length;j++) {
-                    name = name + ' ' + nameToFormat[j].charAt(0).toUpperCase() + nameToFormat[j].slice(1);
-                }
-            } else {
-                if(name.includes('-')) {
-                    for(j=0;j<name.length;j++) {
-                        if(name.charAt(j).includes('-')) {
-                            name = name.substring(0, j+1) + nameToFormat.charAt(j+1).toUpperCase() + nameToFormat.slice(j+2);
+                if(name.includes(' ')) {
+                    for(j=2;j<nameToFormat.length;j++) {
+                        if(name.charAt(j).includes(' ')) {
+                            name = name + ' ' + nameToFormat[j].charAt(0).toUpperCase() + nameToFormat[j].slice(1);
                         }
                     }
                 }
-            }
             return name;
         }
     }

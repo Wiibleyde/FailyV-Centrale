0de1e25ecab74d4e42e9dfebf2010f5b829aad3b
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-20 19:54:32 +0200

    Fixed plate recovery

--- buttons/vehicule/vehEditCt.js
+++ buttons/vehicule/vehEditCt.js
@@ -5,7 +5,7 @@ const logger = require('../../modules/logger');
 
 module.exports = {
     execute: async function(interaction, errEmb) {
-        const vehPlate = interaction.message.content.split(" \u200b- ")[1];
+        const vehPlate = interaction.message.content.split("[0m[2;34m")[2];
         const vehEditCtModal = new ModalBuilder().setCustomId(`vehEditCtModal`).setTitle(`Mettre à jour la date du contrôle technique`);
         const ct = new ActionRowBuilder().addComponents(new TextInputBuilder().setCustomId(`ct`).setLabel(`Nouvelle date du contrôle technique`).setStyle(TextInputStyle.Short).setPlaceholder("Ex: JJ/MM/AAAA").setRequired(true));
         const plate = new ActionRowBuilder().addComponents(new TextInputBuilder().setCustomId(`plate`).setLabel(`Plaque d'immatriculation du véhicule`).setStyle(TextInputStyle.Short).setValue(vehPlate).setPlaceholder("Ex: LSMS 830").setRequired(true));

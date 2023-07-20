8445dad5eb1dbb8c85e465972f15cd1c280c0a6b
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-20 16:59:44 +0200

    Fixed message edition

--- modals/vehicule/vehEditCtModal.js
+++ modals/vehicule/vehEditCtModal.js
@@ -6,6 +6,8 @@ const logger = require('../../modules/logger');
 const sql = require('../../sql/objectsManagement/vehicule');
 //Récup du créateur d'embed
 const emb = require('../../modules/embeds');
+//Fonction pour attendre
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
     execute: async function(interaction, errEmb) {
@@ -50,10 +52,15 @@ module.exports = {
             );
         }
 
-        const message = interaction.channel.messages.fetch(vehicleData[0].messageID);
-        message.edit({ content: `**${vehicleData[0].name} (${vehicleData[0].type}) \u200b- ${plate} \u200b- CT fait le: ${interaction.components[0].components[0].value}**`, components: [vehiclesBtns] });
+        interaction.channel.messages.fetch().then(msg => {
+            msg.map(async d => {
+                if(d.id == vehicleData[0].messageID) {
+                    d.edit({ content: `**${vehicleData[0].name} (${vehicleData[0].type}) \u200b- ${plate} \u200b- CT fait le: ${interaction.components[0].components[0].value}**`, components: [vehiclesBtns] });
+                }
+            });
+        });
         //Send confirmation message
-        await interaction.reply({ embeds: [emb.generate(null, null, `La date du contrôle technique du véhicule immatriculé **${plate}** a bien été mise à jour sur ${interaction.components[0].components[0].value} !`, `#0DE600`, process.env.LSMS_LOGO_V2, null, `Gestion des véhicules`, `https://cdn.discordapp.com/icons/${process.env.IRIS_PRIVATE_GUILD_ID}/${interaction.client.guilds.cache.get(process.env.IRIS_PRIVATE_GUILD_ID).icon}.webp`, null, null, null, false)], ephemeral: true });
+        await interaction.reply({ embeds: [emb.generate(null, null, `La date du contrôle technique du véhicule immatriculé **${plate}** a bien été mise à jour sur **${interaction.components[0].components[0].value}** !`, `#0DE600`, process.env.LSMS_LOGO_V2, null, `Gestion des véhicules`, `https://cdn.discordapp.com/icons/${process.env.IRIS_PRIVATE_GUILD_ID}/${interaction.client.guilds.cache.get(process.env.IRIS_PRIVATE_GUILD_ID).icon}.webp`, null, null, null, false)], ephemeral: true });
         // Supprime la réponse après 5s
         await wait(5000);
         await interaction.deleteReply();

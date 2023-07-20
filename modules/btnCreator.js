35742d2d0ed4eab51c93f44e00fd9e6e6da5b658
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-21 00:35:59 +0200

    Added temp debug to check btn creation

--- modules/btnCreator.js
+++ modules/btnCreator.js
@@ -5,10 +5,13 @@ const logger = require('./logger');
 
 module.exports = {
     genBedsBtns: (buttons) => {
+        logger.debug('Starting beds buttons creation');
         const btns = new ActionRowBuilder();
         for(i=0;i<buttons.length;i++) {
+            logger.debug(`Created "${buttons[i].toUpperCase()}" button`);
             btns.addComponents(new ButtonBuilder().setLabel(buttons[i].toUpperCase()).setCustomId(buttons[i]).setStyle(ButtonStyle.Danger));
         }
+        logger.debug(`End of creation`);
         return btns;
     }
 }

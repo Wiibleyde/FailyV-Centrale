199dabc5d1b00b70f0eb9dfe20818a31ad1c832f
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-07-20 15:50:51 +0200

    Modify ping
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- commands/global/ping.js
+++ commands/global/ping.js
@@ -17,6 +17,6 @@ module.exports = {
         const time = new Date(`${logger.getStartDate()} GMT+2:00`);
         const logged_at = Math.floor(time / 1000);
         //Action lors de l'execution de la commande
-        await interaction.followUp({ embeds: [emb.generate(null, null, `🏓 Pong ! \n- **Ping :** ${waitMsg.createdTimestamp - interaction.createdTimestamp} ms \n- **Latence :** ${Math.round(interaction.client.ws.ping)}ms \n- **Mémoire :** ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100} MB \n- **En ligne depuis : <t:${logged_at}:R>** `, `Gold`, null, null, null, null, null, interaction.client.user.username, interaction.client.user.avatarURL(), true)], ephemeral: true });
+        await interaction.followUp({ embeds: [emb.generate(null, null, `🏓 Pong ! \n- **Ping :** ${Math.round(interaction.client.ws.ping)} ms \n- **Latence :** ${Date.now() - interaction.createdTimestamp} ms \n- **Mémoire :** ${Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100} MB \n- **En ligne depuis : <t:${logged_at}:R>** `, `Gold`, null, null, null, null, null, interaction.client.user.username, interaction.client.user.avatarURL(), true)], ephemeral: true });
     },
 };

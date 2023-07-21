53307ffb6edde363957052aabce8d49f9cb2bf4a
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-21 15:53:39 +0200

    Updated loggers

--- selectMenus/radio/centraleResetRadioSelect.js
+++ selectMenus/radio/centraleResetRadioSelect.js
@@ -10,7 +10,7 @@ const service = require('../../modules/service');
 module.exports = {
     execute: async function(interaction, errEmb) {
         //Logs de quel option du menu de selection à été utilisée
-        logger.log(`${interaction.member.nickname} - ${interaction.user.username}#${interaction.user.discriminator} (\\${interaction.user})\n\na utilisé(e) l'option "${interaction.values}" du menu de séléction "${interaction.customId}"`, interaction.client);
+        logger.log(`${interaction.member.nickname} - ${interaction.user.username}#${interaction.user.discriminator} (${interaction.user})\n\na utilisé(e) l'option "${interaction.values}" du menu de séléction "${interaction.customId}"`, interaction.client);
         
         //Confirmation à Discord du succès de l'opération
         await interaction.deferReply({ ephemeral: true });

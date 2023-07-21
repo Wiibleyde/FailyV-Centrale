e3dbe18443e94c300903c84be3c6e1247141bc9c
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-21 12:30:50 +0200

    Removed debugger

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,7 +6,6 @@ module.exports = {
     async execute(message) {
         if(message.channelId == process.env.IRIS_SERVICE_CHANNEL_ID || message.channelId == process.env.IRIS_RADIO_CHANNEL_ID) {
             if(message.author != process.env.IRIS_DISCORD_ID) {
-                logger.debug(message);
                 logger.log(`${message.author.username}#${message.author.discriminator} (<@${message.author.id}>) à envoyé un message dans un salon interdit (${message.content})`);
                 await message.delete();
             }

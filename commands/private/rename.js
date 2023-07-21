5eb1a56873cfe9c62a60ecbce3606212c57e4ec3
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-21 15:08:59 +0200

    Fix username retrieving

--- commands/private/rename.js
+++ commands/private/rename.js
@@ -34,18 +34,18 @@ module.exports = {
         if(hasAuthorization(Rank.Director, interaction.member.roles.cache)) {
             if(interaction.options.getString(`action`) === `rename`) {
                 const renameModal = new ModalBuilder().setCustomId(`renameModal`).setTitle(`Changer le pseudo`);
-                const pseudo = new ActionRowBuilder().addComponents(new TextInputBuilder().setCustomId(`pseudo`).setLabel(`Pseudo`).setStyle(TextInputStyle.Short).setPlaceholder(`${interaction.client.user.name}`).setRequired(true));
+                const pseudo = new ActionRowBuilder().addComponents(new TextInputBuilder().setCustomId(`pseudo`).setLabel(`Pseudo`).setStyle(TextInputStyle.Short).setPlaceholder(`${interaction.client.user.username}`).setRequired(true));
                 renameModal.addComponents(pseudo);
                 await interaction.showModal(renameModal);
             } else {
                 await sql.clearName();
                 interaction.guild.members.cache.get(interaction.client.id).setNickname('');
-                await interaction.reply({ embeds: [emb.generate(null, null, `Mon nom à bien été réinitialisé !`, "#0DE600", process.env.LSMS_LOGO_V2, null, `Gestion ${interaction.client.user.name}`, `https://cdn.discordapp.com/icons/${process.env.IRIS_PRIVATE_GUILD_ID}/${interaction.client.guilds.cache.get(process.env.IRIS_PRIVATE_GUILD_ID).icon}.webp`, null, null, null, false)], ephemeral: true });
+                await interaction.reply({ embeds: [emb.generate(null, null, `Mon nom à bien été réinitialisé !`, "#0DE600", process.env.LSMS_LOGO_V2, null, `Gestion ${interaction.client.user.username}`, `https://cdn.discordapp.com/icons/${process.env.IRIS_PRIVATE_GUILD_ID}/${interaction.client.guilds.cache.get(process.env.IRIS_PRIVATE_GUILD_ID).icon}.webp`, null, null, null, false)], ephemeral: true });
                 await wait(5000);
                 await interaction.deleteReply();
             }
         } else {
-            await interaction.reply({ embeds: [emb.generate(`Désolé :(`, null, `Vous n'avez pas les permissions suffisantes pour utiliser cette commande. Seul le <@&${process.env.IRIS_DIRECTOR_ROLE}> est autorisé à s'en servir !`, "#FF0000", process.env.LSMS_LOGO_V2, null, `Gestion ${interaction.client.user.name}`, `https://cdn.discordapp.com/icons/${process.env.IRIS_PRIVATE_GUILD_ID}/${interaction.client.guilds.cache.get(process.env.IRIS_PRIVATE_GUILD_ID).icon}.webp`, null, null, null, false)], ephemeral: true });
+            await interaction.reply({ embeds: [emb.generate(`Désolé :(`, null, `Vous n'avez pas les permissions suffisantes pour utiliser cette commande. Seul le <@&${process.env.IRIS_DIRECTOR_ROLE}> est autorisé à s'en servir !`, "#FF0000", process.env.LSMS_LOGO_V2, null, `Gestion ${interaction.client.user.username}`, `https://cdn.discordapp.com/icons/${process.env.IRIS_PRIVATE_GUILD_ID}/${interaction.client.guilds.cache.get(process.env.IRIS_PRIVATE_GUILD_ID).icon}.webp`, null, null, null, false)], ephemeral: true });
             await wait(5000);
             await interaction.deleteReply();
         }

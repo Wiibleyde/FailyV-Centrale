1d87e38c7afeb24cde11fafa444be8d981b8984e
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-22 20:57:42 +0200

    Removed useless await

--- modules/logger.js
+++ modules/logger.js
@@ -24,34 +24,34 @@ logger.level = 'all';
 
 module.exports = {
     log: (log) => {
-        return new Promise(async (resolve, reject) => {
+        return new Promise((resolve, reject) => {
             logger.log(log);
             const embed = emb.generate(null, null, log, `#159879`, process.env.LSMS_LOGO_V2, null, `LOG`, null, null, null, null, true);
-            await webhookClient.send({ embeds: [embed] });
+            webhookClient.send({ embeds: [embed] });
         });
     },
     debug: (debug) => {
-        return new Promise(async (resolve, reject) => {
+        return new Promise((resolve, reject) => {
             logger.debug(debug);
             try {
                 const embed = emb.generate(null, null, debug, `#1688CD`, process.env.LSMS_LOGO_V2, null, `DEBUG`, null, null, null, null, true);
-                await webhookClient.send({ embeds: [embed] });
+                webhookClient.send({ embeds: [embed] });
             } catch (err) {
                 debug = JSON.stringify(debug);
                 if(!debug.includes('https://')) {
                     try {
                         const embed = emb.generate(null, null, debug, `#1688CD`, process.env.LSMS_LOGO_V2, null, `DEBUG`, null, null, null, null, true);
-                        await webhookClient.send({ embeds: [embed] });
+                        webhookClient.send({ embeds: [embed] });
                     } catch (err2) {
                         try {
-                            await webhookClient.send({ content: '**DEBUG:**\n```\n' + debug + '\n```' });
+                            webhookClient.send({ content: '**DEBUG:**\n```\n' + debug + '\n```' });
                         } catch (err3) {
                             logger.error(err3);
                         }
                     }
                 } else {
                     try {
-                        await webhookClient.send({ content: '**DEBUG:**\n```\n' + debug + '\n```' });
+                        webhookClient.send({ content: '**DEBUG:**\n```\n' + debug + '\n```' });
                     } catch (err2) {
                         logger.error(err2);
                     }
@@ -60,17 +60,17 @@ module.exports = {
         });
     },
     warn: (warn) => {
-        return new Promise(async (resolve, reject) => {
+        return new Promise((resolve, reject) => {
             logger.warn(warn);
             const embed = emb.generate(null, null, warn, `#FFD800`, process.env.LSMS_LOGO_V2, null, `WARN`, null, null, null, null, true);
-            await webhookClient.send({ embeds: [embed] });
+            webhookClient.send({ embeds: [embed] });
         });
     },
     error: (error) => {
-        return new Promise(async (resolve, reject) => {
+        return new Promise((resolve, reject) => {
             logger.error(error);
             const embed = emb.generate(null, null, error, `#FF0000`, process.env.LSMS_LOGO_V2, null, `ERROR`, null, null, null, null, true);
-            await webhookClient.send({ embeds: [embed] });
+            webhookClient.send({ embeds: [embed] });
         });
     },
     getStartDate: () => {

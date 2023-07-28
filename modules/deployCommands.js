d29a7e81186747a1392d98677545dca6e44136c1
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-28 12:02:38 +0200

    Removed useless commands split

--- modules/deployCommands.js
+++ modules/deployCommands.js
@@ -11,26 +11,13 @@ module.exports = {
         //Init de arrays pour stocker les commandes en fonction de où elles doivent être envoyés
         const publicCommands = [];
         const privateCommands = [];
-        const debugCommands = [];
         //Récup du chemin des fichiers de commandes
         const foldersPath = path.join(__dirname, '../commands');
         const commandsFolders = fs.readdirSync(foldersPath);
         
         //Stockage des commandes dans les différentes arrays
         for(const folder of commandsFolders) {
-            if(folder == 'test') {
-                const commandsPath = path.join(foldersPath, folder);
-                const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-                for(const file of commandFiles) {
-                    const filePath = path.join(commandsPath, file);
-                    const command = require(filePath);
-                    if('data' in command && 'execute' in command) {
-                        debugCommands.push(command.data.toJSON());
-                    } else {
-                        logger.warn(`La commande ${filePath} n'a pas la partie "data" ou "execute" !`);
-                    }
-                }
-            } else if(folder == 'private') {
+            if(folder == 'private') {
                 const commandsPath = path.join(foldersPath, folder);
                 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
                 for(const file of commandFiles) {
@@ -61,16 +48,13 @@ module.exports = {
         
         (async () => {
             try {
-                logger.log(`Rechargement de ${debugCommands.length} commandes slash (/) de dev.`);
                 logger.log(`Rechargement de ${privateCommands.length} commandes slash (/) privées.`);
                 logger.log(`Rechargement de ${publicCommands.length} commandes slash (/) publiques.`);
                 //Envois des commandes sur un serveur spécifique
                 const privateData = await rest.put(Routes.applicationGuildCommands(process.env.IRIS_DISCORD_ID, process.env.IRIS_PRIVATE_GUILD_ID), { body: privateCommands });
-                //Envois des commandes sur le serveur de dev
-                const debugData = await rest.put(Routes.applicationGuildCommands(process.env.IRIS_DISCORD_ID, process.env.IRIS_DEBUG_GUILD_ID), { body: debugCommands });
                 //Envois des commandes globalement
                 const publicData = await rest.put(Routes.applicationCommands(process.env.IRIS_DISCORD_ID), { body: publicCommands });
-                logger.log(`Rechargement de ${privateData.length + publicData.length + debugData.length} commandes slash (/) réussies.`);
+                logger.log(`Rechargement de ${privateData.length + publicData.length} commandes slash (/) réussies.`);
             } catch (error) {
                 logger.error(error);
             }

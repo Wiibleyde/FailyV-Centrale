4c524774b032ebd1f40a1b8de52b9c98d1ada2dd
ailedeuxplume <ailedeuxplume@gmail.com>
2023-07-30 11:12:55 +0200

    Auto calculated expire date when not specified

--- commands/private/organes.js
+++ commands/private/organes.js
@@ -137,10 +137,40 @@ module.exports = {
             date = nowDate.getFullYear() + '-' + date[1] + '-' + date[0] + ' 00:00:00';
         } else {
             let day = nowDate.getDate();
-            if (day < 10) day = '0' + day;
+            day = day + 14;
             let month = nowDate.getMonth() + 1;
+            let year = nowDate.getFullYear();
+            if(month == 12) {
+                if(day > 31) {
+                    day = day - 31;
+                    month = 1;
+                    year ++;
+                }
+            } else if(month == 2) {
+                if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+                    if(day > 29) {
+                        day = day - 29;
+                        month ++;
+                    }
+                } else {
+                    if(day > 28) {
+                        day = day - 28;
+                        month ++;
+                    }
+                }
+            } else if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10) {
+                if(day > 31) {
+                    day = day - 31;
+                    month ++;
+                }
+            } else {
+                if(day > 30) {
+                    day = day - 30;
+                    month ++;
+                }}
+            if (day < 10) day = '0' + day;
             if (month < 10) month = '0' + month;
-            date = nowDate.getFullYear() + '-' + month + '-' + day + ' 00:00:00';
+            date = year + '-' + month + '-' + day + ' 00:00:00';
         }
 
         for(i=0;i<quantity;i++) {

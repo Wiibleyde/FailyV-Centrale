52f593389e88befdf3b4a87f0a5cb54cfee6275f
Nicolas <34538739+TheNicolas190@users.noreply.github.com>
2023-08-23 00:57:05 +0200

    fix: set date format in organe date description to JJ/MM

--- commands/private/organes.js
+++ commands/private/organes.js
@@ -70,7 +70,7 @@ module.exports = {
             .setRequired(false)
         ).addStringOption(option =>
             option.setName(`date`)
-            .setDescription(`Définir une date de péremption pour l'organe (format: JJ/MM/AAAA) (Laisser vide la date du jour)`)
+            .setDescription(`Définir une date de péremption pour l'organe (format: JJ/MM) (Laisser vide la date du jour)`)
             .setMinLength(5)
             .setMaxLength(5)
             .setRequired(false)

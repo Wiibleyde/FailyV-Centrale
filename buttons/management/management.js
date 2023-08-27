7c2592cb81749e4842cdd4467aa8b49511196e88
ailedeuxplume <ailedeuxplume@gmail.com>
2023-08-28 00:10:30 +0200

    Add security on regen

--- buttons/management/management.js
+++ buttons/management/management.js
@@ -1,3 +1,7 @@
+const service = require('../../modules/service');
+const emb = require('../../modules/embeds');
+const wait = require('node:timers/promises').setTimeout;
+
 module.exports = {
     execute: async function(interaction, errEmb) {
         if(interaction.customId == 'managementDebug') {
@@ -7,6 +11,14 @@ module.exports = {
             const workforce = require('../../modules/regenerateWorkforce');
             workforce.execute(interaction);
         } else if(interaction.customId == 'managementBlackout') {
+            if(service.isGen()) {
+                const title = `BLACKOUT`;
+                const serverIcon = `https://cdn.discordapp.com/icons/${interaction.guild.id}/${interaction.guild.icon}.webp`;
+                const embed = emb.generate(null, null, `Désolé, il y a déjà quelque chose en cours de régénération !`, `#FF0000`, process.env.LSMS_LOGO_V2, null, title, serverIcon, null, null, null, false);
+                await interaction.reply({ embeds: [embed], ephemeral: true });
+                await wait(5000);
+                await interaction.deleteReply();
+            }
             const blackout = require('../../modules/blackout');
             blackout.execute(interaction);
         }
